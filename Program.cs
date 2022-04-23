2437343f86dd5d19163c4739f3ff1ea71beb676a
renangeraldosilva <renangeraldosilva@gmail.com>
2022-04-23 16:18:57 -0300

    Exercicio 91 com vetores

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio91
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[50]; 
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine($"Digite o {i+1}° numero");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                for (int j = i + 1; j < vet.Length; j++)
+                {
+                    if (vet[i] == vet[j])
+                    {
+                        Console.WriteLine($"Numeros repitidos nas posições {i} e {j}- numero repitido {vet[i]}");
+                    }
+                }
+            }
+        }
+    }
+}
